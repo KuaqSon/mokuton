1a48a62baa287e7df7e6d93811d61ec5f9d6661f
KuaqSon <qson.ng@gmail.com>
2021-11-21 15:44:16 +0700

    more tweak

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-gunicorn mokuton.wsgi:application --bind 0.0.0.0:8000
+gunicorn mokuton.wsgi:application --bind 0.0.0.0:8000 --timeout 30 --workers 4 --keep-alive 1 --log-level=debug
