b70d98c252994dbf2f5795cb445e06d6add8af52
kuaqson <qson.ng@gmail.com>
2022-05-03 17:18:05 +0700

    Setup Huey docker

---
+++ docker/django/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+python manage.py migrate --no-input
+python manage.py installwatson
+python manage.py collectstatic --no-input
+
+gunicorn mokuton.wsgi:application --bind 0.0.0.0:8000 --timeout 30 --workers 4 --keep-alive 1 --log-level=debug
