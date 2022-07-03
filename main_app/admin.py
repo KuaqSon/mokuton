647806f569e96e6a988253f32990ed013753bb6e
kuaqson <qson.ng@gmail.com>
2022-07-03 17:57:53 +0700

    user admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
+from main_app.models import User
+
+
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    list_display = (
+        "name",
+        "email",
+        "is_active",
+        "created_at",
+    )
+
+    fields = (
+        "name",
+        "email",
+        "time_zone",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    )
+    readonly_fields = ["is_superuser"]
+
+
+admin.site.register(User, UserAdmin)
